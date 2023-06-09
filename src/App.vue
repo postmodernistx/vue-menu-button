9e6804fe9c010bca32c5db058c9f7bbfb95046d0
Jenni <jenni@ohsomodern.org>
2023-06-09 21:08:48 +0300

    :sparkles: feat(app): add button & menu

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <script  lang="ts" setup="">
+import MenuButton from './components/MenuButton.vue';
+import MainNavigation from './fixtures/MainNavigation.vue';
+import { ref } from 'vue';
 
+const isMenuOpen = ref(false);
+function handleMenuState(isOpen: boolean) {
+  // Listen to @toggle event from MenuButton component
+  // The event passes a value (isOpen), which we then
+  // apply to the ref isMenuOpen so that we can pass it down
+  // to MainNavigation component
+  isMenuOpen.value = isOpen;
+}
 </script>
 
 <template>
- <div>
-
- </div>
+  <div>
+    <header>
+      <MenuButton @toggle="handleMenuState" />
+      <MainNavigation :is-open="isMenuOpen" />
+    </header>
+  </div>
 </template>
-
-<style lang="scss" scoped>
-
-</style>
