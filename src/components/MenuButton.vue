ce8d58be02ec4ce52ea792d41cce392fb653e36f
Jenni <jenni@ohsomodern.org>
2023-06-09 21:08:12 +0300

    :sparkles: feat(button): add menu button

---
+++ src/components/MenuButton.vue
@@ -0,0 +1,18 @@
+<script  lang="ts" setup="">
+import { ref } from 'vue';
+
+const emit = defineEmits(['toggle']);
+const isOpen = ref(false);
+
+function handleClick() {
+  isOpen.value = !isOpen.value; // Toggle state
+  emit('toggle', isOpen.value); // Emit current state to parent component
+}
+</script>
+
+<template>
+  <button @click="handleClick">
+    <span v-if="isOpen">Close menu</span>
+    <span v-if="!isOpen">Open menu</span>
+  </button>
+</template>
